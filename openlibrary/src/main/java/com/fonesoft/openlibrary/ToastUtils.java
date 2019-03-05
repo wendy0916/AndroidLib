28dc2cbda469825b6e74ac110564abd657b72375
wendy <1053245119@qq.com>
2019-03-05 14:48:55 +0800

    修改弹框类

--- openlibrary/src/main/java/com/fonesoft/openlibrary/ToastUtils.java
+++ openlibrary/src/main/java/com/fonesoft/openlibrary/ToastUtils.java
@@ -1,64 +1,50 @@
 package com.fonesoft.openlibrary;
 
 import android.content.Context;
-import android.os.Handler;
 import android.widget.Toast;
 
 public class ToastUtils {
-    private Handler handler = new Handler();
-    private Toast toast = null;
-    private Object synObj = new byte[0];
-    private Context context;
+    private Toast mToast;
+    private static ToastUtils mToastUtils;
 
-    private ToastUtils() {
+    private ToastUtils(Context context) {
+        mToast = Toast.makeText(context.getApplicationContext(), null, Toast.LENGTH_SHORT);
     }
 
-    private static class SingletonHolder {
-        static ToastUtils sInstance = new ToastUtils();
-    }
-
-    public static ToastUtils getInstance() {
-        return ToastUtils.SingletonHolder.sInstance;
-    }
-
-    public void init(Context context) {
-        this.context = context;
-    }
-
-    public void showMessage(final int resId) {
-        String msg = context.getString(resId);
-        showMessage(msg, Toast.LENGTH_LONG);
-    }
-
-    public void showMessage(final String msg) {
-        showMessage(msg, Toast.LENGTH_LONG);
-    }
-
-    public void showMessage(final String msg,
-                            final int len) {
-        new Thread(new Runnable() {
-            public void run() {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        synchronized (synObj) {
-                            if (toast == null) {
-                                toast = Toast.makeText(context, msg, len);
-                            }
-                            toast.setText(msg);
-                            //toast.setGravity(Gravity.CENTER, 0, 0);
-                            toast.setDuration(len);
-                            toast.show();
-                        }
-                    }
-                });
-            }
-        }).start();
-    }
-
-    public void cancelCurrentToast() {
-        if (toast != null) {
-            toast.cancel();
+    public static synchronized ToastUtils getInstanc(Context context) {
+        if (null == mToastUtils) {
+            mToastUtils = new ToastUtils(context);
         }
+        return mToastUtils;
+    }
+    /**
+     * 显示toast
+     *
+     * @param toastMsg
+     */
+    public void showToast(int toastMsg) {
+        mToast.setText(toastMsg);
+        mToast.show();
+    }
+
+    /**
+     * 显示toast
+     *
+     * @param toastMsg
+     */
+    public void showToast(String toastMsg) {
+        mToast.setText(toastMsg);
+        mToast.show();
+    }
+
+    /**
+     * 取消toast，在activity的destory方法中调用
+     */
+    public void destory() {
+        if (null != mToast) {
+            mToast.cancel();
+            mToast = null;
+        }
+        mToastUtils = null;
     }
 }
